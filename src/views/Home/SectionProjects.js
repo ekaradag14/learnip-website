288de3c36566181bfe50a0af882f76909ab98bd9
Erencan KARADAĞ <64350262+ekaradag14@users.noreply.github.com>
2021-11-13 18:19:51 +0300

    Design Improvements

--- src/views/Home/SectionProjects.js
+++ src/views/Home/SectionProjects.js
@@ -10,6 +10,7 @@ import Timeline from '@material-ui/icons/Timeline';
 import Group from '@material-ui/icons/Group';
 
 // core components
+import { Grid } from '@material-ui/core';
 import GridContainer from 'components/Grid/GridContainer.js';
 import GridItem from 'components/Grid/GridItem.js';
 import Card from 'components/Card/Card.js';
@@ -22,10 +23,17 @@ import Rellax from 'rellax';
 import cardProject2 from 'assets/img/examples/card-project2.jpg';
 import cardProject5 from 'assets/img/examples/card-project5.jpg';
 import writingAiImage from 'assets/novus/writingInTheAgeOfAI.png';
-import Sun from 'assets/learnip/sun.svg';
+import ResultMockDesktop from 'assets/learnip/result_mock.svg';
+import ResultMockMobile from 'assets/learnip/results_mockup_mobile.svg';
+import TurnipBaby from 'assets/learnip/turnip_baby.svg';
+import WaterSearch from 'assets/learnip/water_search.svg';
+import WaterFlags from 'assets/learnip/water_with_flags.svg';
+import RocketTurnip from 'assets/learnip/rocket_turnip.svg';
+import TurnipCool from 'assets/learnip/turnip_cool.svg';
 import FallingTurnip from 'assets/learnip/turnip_crying.svg';
 import HalfBrowserMockup from 'assets/learnip/half_browser_mockup.svg';
 import WaterDroplet from 'assets/learnip/water_droplet.svg';
+import WaterStop from 'assets/learnip/water_stop.svg';
 import Cloud from 'assets/learnip/cloud.svg';
 import Communication from 'assets/novus/Communication.png';
 const useStyles = makeStyles(projectsStyle);
@@ -40,50 +48,72 @@ export default function SectionProjects({ ...rest }) {
 			<div className={classes.projects + ' ' + classes.projects4}>
 				<div className={classes.container}>
 					<GridContainer>
-						<GridItem
-							style={{ textAlign: 'center' }}
-							xs={12}
-							sm={12}
-							md={12}
-							className={classes.mlAuto}
-						>
+						<GridItem style={{ textAlign: 'center' }} xs={12} sm={12} md={12}>
 							<h2>
 								<b>Personalize</b>
 							</h2>
 						</GridItem>
-						<GridItem xs={12} sm={5} md={5} className={classes.mlAuto}>
-							<InfoArea
-								className={classes.info4}
-								title="Choose Languages"
-								description="Choose from many languages provided (More to come) and start learning right away. Become a turnipster to practice multiple languages at once."
-								icon={Timeline}
-								iconColor="rose"
-							/>
-							<InfoArea
-								className={classes.info4}
-								title="Factual"
-								description="AI cannot outperform humans for fact-checking. Yet. Check your facts with our novel Novus Crawler. Arguments in your article like quotes and metrics are automatically searched in trusted sources to un cover the truth. Then you get to choose what is informative about your content."
-								icon={Code}
-								iconColor="success"
-							/>
-							<InfoArea
-								className={classes.info4}
-								title="Personal"
-								description="Every creator has their own unique style. After a few articles, our models adapt to your writing style by using the latest advancements in AI. Yes, AI is going to write stories like you do."
-								icon={Group}
-								iconColor="info"
-							/>
-						</GridItem>
-						<GridItem xs={12} sm={5} md={5} className={classes.mrAuto}>
-							<img
-								src={HalfBrowserMockup}
+						<Grid
+							container
+							style={{
+								display: 'flex',
+								flexDirection: 'row',
+								justifyContent: 'space-between',
+							}}
+						>
+							<GridItem xs={12} sm={6} md={6} style={{ margin: 'auto' }}>
+								<InfoArea
+									style={{ maxWidth: 'none' }}
+									className={classes.info4}
+									isIconSVG
+									iconWidth="70px"
+									iconHeight="70px"
+									icon={RocketTurnip}
+									title="Study at Your Own Pace"
+									description="AI cannot outperform humans for fact-checking. Yet. Check your facts with our novel Novus Crawler. Arguments in your article like quotes and metrics are automatically searched in trusted sources to un cover the truth. Then you get to choose what is informative about your content."
+									iconColor="rose"
+								/>
+								<InfoArea
+									className={classes.info4}
+									isIconSVG
+									iconWidth="70px"
+									iconHeight="70px"
+									icon={WaterFlags}
+									title="Choose Languages"
+									description="Choose from many languages provided (More to come) and start learning right away. Become a turnipster to practice multiple languages at once."
+									iconColor="success"
+								/>
+								<InfoArea
+									className={classes.info4}
+									isIconSVG
+									title="Grow Your Turnip"
+									iconWidth="70px"
+									iconHeight="70px"
+									icon={TurnipBaby}
+									description="Every creator has their own unique style. After a few articles, our models adapt to your writing style by using the latest advancements in AI. Yes, AI is going to write stories like you do."
+									icon={TurnipBaby}
+									iconColor="info"
+								/>
+							</GridItem>
+							<GridItem
+								xs={12}
+								sm={5}
+								md={5}
 								style={{
-									height: 500,
-									marginTop: window.innerWidth > 550 ? 130 : 0,
-									borderRadius: 15,
+									display: 'flex',
+									flexDirection: 'column',
+									justifyContent: 'center',
 								}}
-							/>
-						</GridItem>
+							>
+								<img
+									src={HalfBrowserMockup}
+									style={{
+										borderRadius: 15,
+										padding: 40,
+									}}
+								/>
+							</GridItem>
+						</Grid>
 					</GridContainer>
 					<hr />
 					<img
@@ -91,11 +121,10 @@ export default function SectionProjects({ ...rest }) {
 						data-rellax-speed="7"
 						src={Cloud}
 						style={{
-							width: '10%',
-
+							width: '8%',
 							maxWidth: 150,
 							marginLeft: 'auto',
-							right: '3%',
+							right: '0.6%',
 
 							position: 'absolute',
 						}}
@@ -106,11 +135,11 @@ export default function SectionProjects({ ...rest }) {
 						data-rellax-speed="4"
 						src={FallingTurnip}
 						style={{
-							width: '13%',
+							width: '10%',
 							maxWidth: 200,
 							marginLeft: 'auto',
 							marginTop: -200,
-							right: '10%',
+							right: '5%',
 							position: 'absolute',
 						}}
 					/>
@@ -120,72 +149,89 @@ export default function SectionProjects({ ...rest }) {
 						data-rellax-speed="10"
 						src={Cloud}
 						style={{
-							width: '13%',
+							width: '10%',
 							maxWidth: 200,
 							marginLeft: 'auto',
-							right: '20%',
+							right: '11%',
 							marginTop: 400,
 							position: 'absolute',
 						}}
 					/>
 
 					<GridContainer>
-						<GridItem
-							xs={12}
-							sm={12}
-							md={12}
-							style={{ textAlign: 'center' }}
-							className={classes.mlAuto}
+						<GridItem xs={12} sm={12} md={12} style={{ textAlign: 'center' }}>
+							<h2>
+								<b>Play</b>
+							</h2>
+						</GridItem>
+						<Grid
+							container
+							style={{
+								display: 'flex',
+								flexDirection: 'row',
+								justifyContent: 'space-between',
+							}}
 						>
-							<h2>Communication</h2>
-						</GridItem>
-						<GridItem xs={12} sm={5} md={5} className={classes.mlAuto}>
-							<img
-								src={Communication}
-								style={{
-									height: 300,
-									marginTop: window.innerWidth > 550 ? 130 : 0,
-									borderRadius: 5,
-									marginLeft: window.innerWidth > 550 ? -100 : 0,
-								}}
-								alt="Novus Writer Product Screenshot"
-							/>
-						</GridItem>
-						<GridItem xs={12} sm={5} md={5} className={classes.mrAuto}>
-							<InfoArea
-								className={classes.info4}
-								title="Develop Marketing Campaigns"
-								description="Using truly digital marketing by simply describing the product in a sentence and with some keywords, enabled by Novus AI technologies."
-								icon={FormatPaint}
-								iconColor="info"
-							/>
-							<InfoArea
-								className={classes.info4}
-								title="Create Novel Blog Contents"
-								description="In seconds related to the firm's field. Just input your area of interests and wait for proposed outlines or new ideas. Variety of blog contents related with your input is right there in seconds. "
-								icon={Code}
-								iconColor="primary"
-							/>
-							<InfoArea
-								className={classes.info4}
-								title="Benefit From Resources Which Are Directly Relevant To Your Content"
-								description="We help content creators to increase their precision by making relevant information on the web instantly accessible through suggesting informative paragraphs while they write.
+							<GridItem
+								xs={12}
+								sm={5}
+								md={5}
+								style={{ display: 'flex', flexDirection: 'column' }}
+							>
+								<img
+									src={ResultMockMobile}
+									style={{
+										marginTop: window.innerWidth > 1250 ? 130 : 0,
+										margin: 'auto',
+										borderRadius: 5,
+										padding: 40,
+									}}
+									alt="Novus Writer Product Screenshot"
+								/>
+							</GridItem>
+							<GridItem xs={12} sm={7} md={7}>
+								<InfoArea
+									className={classes.info4}
+									title="Find Your Droplets All Over The Internet"
+									isIconSVG
+									iconWidth="70px"
+									iconHeight="70px"
+									description="Using truly digital marketing by simply describing the product in a sentence and with some keywords, enabled by Novus AI technologies."
+									icon={WaterSearch}
+									iconColor="info"
+								/>
+								<InfoArea
+									className={classes.info4}
+									title="Translate From All Languages"
+									isIconSVG
+									iconWidth="70px"
+									iconHeight="70px"
+									description="In seconds related to the firm's field. Just input your area of interests and wait for proposed outlines or new ideas. Variety of blog contents related with your input is right there in seconds. "
+									icon={TurnipCool}
+									iconColor="primary"
+								/>
+								<InfoArea
+									className={classes.info4}
+									title="Disable If You Feel Like It"
+									isIconSVG
+									iconWidth="70px"
+									iconHeight="70px"
+									description="We help content creators to increase their precision by making relevant information on the web instantly accessible through suggesting informative paragraphs while they write.
 "
-								icon={Dashboard}
-								iconColor="danger"
-							/>
-						</GridItem>
-
+									icon={WaterStop}
+									iconColor="danger"
+								/>
+							</GridItem>
+						</Grid>
 						<img
 							className="rellax"
 							data-rellax-speed="2"
 							src={Cloud}
 							style={{
-								width: '10%',
-
+								width: '7%',
 								maxWidth: 150,
 								marginLeft: 'auto',
-								left: '12.5%',
+								left: '3%',
 								marginTop: 400,
 								position: 'absolute',
 							}}
@@ -196,11 +242,11 @@ export default function SectionProjects({ ...rest }) {
 							data-rellax-speed="10"
 							src={WaterDroplet}
 							style={{
-								width: '13%',
+								width: '10%',
 								maxWidth: 200,
 								marginLeft: 'auto',
 								marginTop: 1500,
-								left: '3%',
+								left: '5%',
 								position: 'absolute',
 							}}
 						/>
